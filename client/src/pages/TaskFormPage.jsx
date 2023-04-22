a2805e8a9824e16e74daae709799ac8c247a7266
Brayant Gualdron <zbgualdron@gmail.com>
2023-04-22 18:08:26 -0500

    Delete task

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,5 +1,5 @@
 import { useForm } from 'react-hook-form'
-import { createTask } from '../api/tasks.api'
+import { createTask, deleteTask } from '../api/tasks.api'
 import { useNavigate, useParams } from 'react-router-dom'
 
 export function TasksFormPage() {
@@ -28,6 +28,17 @@ export function TasksFormPage() {
 
             <button> Save </button>
         </form>
+
+        {
+            params.id && <button onClick={async () => {
+                const accepted = window.confirm('Are you sure')
+                if(accepted){
+                    await deleteTask(params.id)
+                    navigate("/tasks")
+                }
+            }}> Delete </button>
+        }
+
         </div>
     )
 }
