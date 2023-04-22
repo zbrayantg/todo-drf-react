91e8ea52c4f566b8fee72ddb596420f1a9ddbb89
Brayant Gualdron <zbgualdron@gmail.com>
2023-04-22 17:19:00 -0500

    Setting for task list components, pages and navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,18 @@
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import { TasksPage } from './pages/TasksPage'
+import { TasksFormPage } from './pages/TaskFormPage'
+import { Navigation } from './components/Navigation'
+
 function App(){
   return (
-    <div>Hello World</div>
+    <BrowserRouter>
+      <Navigation/>
+      <Routes>
+        <Route path="/" element={ <Navigate to="/tasks"/>}/>
+        <Route path="/tasks" element={<TasksPage/>}/>
+        <Route path="/tasks-create" element={<TasksFormPage/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
